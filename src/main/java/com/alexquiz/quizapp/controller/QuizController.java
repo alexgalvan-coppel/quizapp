6d6f93e1c4cf9a966982e70646e9fc384cb1067d
alexgalvan-coppel <edgar.galvan@coppel.com>
2026-07-24 16:10:59 -0600

    add question
    update system, now I can add questions

---
+++ src/main/java/com/alexquiz/quizapp/controller/QuizController.java
@@ -0,0 +1,25 @@
+package com.alexquiz.quizapp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alexquiz.quizapp.service.QuizService;
+
+
+@RestController
+@RequestMapping("quiz")
+public class QuizController {
+
+    @Autowired
+    QuizService quizService;
+    
+    @PostMapping("create")
+    public ResponseEntity<String> createQuiz(@RequestParam String category,@RequestParam int numQ, @RequestParam String title){
+        return quizService.createQuiz(category,numQ,title);
+    }
+
+}
--- src/main/java/com/alexquiz/quizapp/dao/QuestionDao.java
+++ src/main/java/com/alexquiz/quizapp/dao/QuestionDao.java
@@ -3,6 +3,7 @@ package com.alexquiz.quizapp.dao;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.alexquiz.quizapp.entities.Question;
 
@@ -11,5 +12,8 @@ import com.alexquiz.quizapp.entities.Question;
 public interface  QuestionDao extends JpaRepository<Question, Integer> {
 
     List<Question> findByCategory(String category);
+
+    @Query(value = "SELECT q.id FROM question q Where q.category=:category ORDER BY RANDOM() LIMIT :numQ", nativeQuery = true)
+    List<Question> findRandomQuestionsByCategory(String category, int numQ);
     
 }
